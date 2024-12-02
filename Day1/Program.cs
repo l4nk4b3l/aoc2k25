eb7c7407cbcb93b50d6897b9a4964ae4a0a180b3
l4nk4b3l <github@l4nk4b3l.de>
2024-12-02 21:15:52 +0100

    Day1 in C#

---
+++ Day1/Program.cs
@@ -0,0 +1,45 @@
+﻿Part1();
+Part2();
+
+void Part1()
+{
+    var left = new List<int>();
+    var right = new List<int>();
+
+    foreach (string line in File.ReadLines("./input.txt"))
+    {
+        string[] splitted = line.Split("   ");
+    
+        left.Add(int.Parse(splitted[0]));
+        right.Add(int.Parse(splitted[1]));
+    }
+
+    left.Sort();
+    right.Sort();
+    
+    int sum = 0;
+    for (int i = 0; i < left.Count; i++) 
+        sum += Math.Abs(left.ElementAt(i) - right.ElementAt(i));
+    
+    Console.WriteLine($"Part 1: {sum}");
+}
+
+void Part2()
+{
+    var left = new List<int>();
+    var right = new List<int>();
+
+    foreach (string line in File.ReadLines("./input.txt"))
+    {
+        string[] splitted = line.Split("   ");
+    
+        left.Add(int.Parse(splitted[0]));
+        right.Add(int.Parse(splitted[1]));
+    }
+
+    var lookup = right.ToLookup(x => x);
+    
+    int sum = left.Sum(d=> d * lookup[d].Count());
+
+    Console.WriteLine($"Part 2: {sum}");
+}
