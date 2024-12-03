f28367d9bb776b5563797f1e9810dabc40320bf7
l4nk4b3l <github@l4nk4b3l.de>
2024-12-03 21:48:13 +0100

    day3 part 1

---
+++ Day3/Program.cs
@@ -0,0 +1,19 @@
+﻿using System.Text.RegularExpressions;
+
+namespace Day3;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Part1();
+    }
+
+    static void Part1()
+    {
+        string input = File.ReadAllText("./input.txt");
+        MatchCollection matches = Regex.Matches(input, @"mul\((\d+),(\d+)\)");
+        int sum = matches.Select(d => int.Parse(d.Groups[1].Value) * int.Parse(d.Groups[2].Value)).Sum();
+        Console.WriteLine(sum);
+    }
+}
