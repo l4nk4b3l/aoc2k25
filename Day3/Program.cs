0e09d23e807b9ac032d57a59a4a4d504416a0652
l4nk4b3l <github@l4nk4b3l.de>
2024-12-03 22:04:21 +0100

    day 3 part 2

--- Day3/Program.cs
+++ Day3/Program.cs
@@ -1,4 +1,5 @@
-﻿using System.Text.RegularExpressions;
+﻿using System.Text;
+using System.Text.RegularExpressions;
 
 namespace Day3;
 
@@ -7,12 +8,26 @@ class Program
     static void Main(string[] args)
     {
         Part1();
+        Part2();
     }
 
     static void Part1()
     {
         string input = File.ReadAllText("./input.txt");
         MatchCollection matches = Regex.Matches(input, @"mul\((\d+),(\d+)\)");
+        int sum = matches.Select(d => int.Parse(d.Groups[1].Value) * int.Parse(d.Groups[2].Value)).Sum();
+        Console.WriteLine(sum);
+    }
+
+    static void Part2()
+    { 
+        string input = File.ReadAllText("./input.txt");
+        string[] parts = input.Split("do()");
+        StringBuilder stringBuilder = new StringBuilder();
+        foreach (string part in parts) 
+            stringBuilder.Append(part.Split("don't()")[0]);
+        
+        MatchCollection matches = Regex.Matches(stringBuilder.ToString(), @"mul\((\d+),(\d+)\)");
         int sum = matches.Select(d => int.Parse(d.Groups[1].Value) * int.Parse(d.Groups[2].Value)).Sum();
         Console.WriteLine(sum);
     }
